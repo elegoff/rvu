307f661a2b046e906a7365b6c31e73f1dc0a8935
Eric Le Goff <elegoff@protonmail.com>
2022-09-14 10:24:43 +0200

    Calculate the image sizing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::cmp::max;
+
 use clap::Parser;
 use thiserror::Error;
 use winit::{
+    dpi::PhysicalSize,
     error::OsError,
     event::{ElementState, KeyboardInput, VirtualKeyCode},
     event_loop::{ControlFlow, EventLoop},
@@ -11,6 +14,12 @@ use winit::{
 enum RvuError {
     #[error("Unable to create window")]
     WindowError(#[from] OsError),
+    #[error("Failed to process image")]
+    ImageError(#[from] image::ImageError),
+    #[error("Failed to load image")]
+    IOError(#[from] std::io::Error),
+    #[error("Unable to calculate screen size")]
+    NoPrimaryMonitor,
 }
 
 #[derive(Debug, Parser)]
@@ -22,14 +31,37 @@ struct Config {
 
 type Result<T> = std::result::Result<T, RvuError>;
 
+const SCREEN_PERCENT: u32 = 90;
+
 fn main() -> Result<()> {
     let config = Config::parse();
-    println!("The filename is {}", config.file_name);
+
+    let image = image::io::Reader::open(config.file_name)?.decode()?;
+
+    let w = image.width();
+    let h = image.height();
+
+    println!("image is {} by {}", w, h);
 
     let event_loop = EventLoop::new();
+    let primary_monitor = event_loop
+        .primary_monitor()
+        .ok_or(RvuError::NoPrimaryMonitor)?;
+
+    let screen_size = primary_monitor.size();
+    let max_screen_size = (
+        screen_size.width * SCREEN_PERCENT / 100,
+        screen_size.height * SCREEN_PERCENT / 100,
+    );
+    let horz_scale = calc_scale(max_screen_size.0, image.width());
+    let vert_scale = calc_scale(max_screen_size.1, image.height());
+    let scale = max(horz_scale, vert_scale);
+
+    let window_inner_size = PhysicalSize::new(image.width() / scale, image.height() / scale);
 
     let window = WindowBuilder::new()
         .with_title("My image viewer")
+        .with_inner_size(window_inner_size)
         .build(&event_loop)?;
 
     event_loop.run(move |event, _, control_flow| {
@@ -63,4 +95,12 @@ fn main() -> Result<()> {
             _ => (),
         }
     });
+}
+
+fn calc_scale(max_size: u32, cur_size: u32) -> u32 {
+    if max_size > cur_size {
+        1
+    } else {
+        ((cur_size as f32) / (max_size as f32)).ceil() as u32
+    }
 }
