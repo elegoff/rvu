199931166b19de9faae255007625c949facaddf1
Eric Le Goff <elegoff@protonmail.com>
2022-09-14 09:29:08 +0200

    Opening a window via winit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rvu
+This is a Rust project from a trabscription of that video
+
+https://www.youtube.com/watch?v=1yofBPRx864
+
+from the "Rust and C++ Cardiff" meetup (https://www.meetup.com/rust-and-c-plus-plus-in-cardiff/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use winit::{
+    event::{ElementState, KeyboardInput, VirtualKeyCode},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_title("RVU")
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            winit::event::Event::WindowEvent { window_id, event } if window_id == window.id() => {
+                match event {
+                    winit::event::WindowEvent::Resized(_) => (),
+
+                    winit::event::WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+
+                    winit::event::WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+
+                    winit::event::WindowEvent::ScaleFactorChanged {
+                        scale_factor: _,
+                        new_inner_size: _,
+                    } => todo!(),
+                    _ => (),
+                }
+            }
+            winit::event::Event::RedrawRequested(_) => (),
+            _ => (),
+        }
+    });
 }
